0e5762d565473524753c9ad046079d98d5ebfca4
ceno118 <ceno118@gmail.com>
2023-04-13 15:22:17 -0400

    added wall textures

--- object_renderer.py
+++ object_renderer.py
@@ -7,6 +7,14 @@ class ObjectRenderer:
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
 
+    def draw(self):
+        self.render_game_objects()
+
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
     @staticmethod
     def get_texture(path, res = (TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
